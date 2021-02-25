e7f4211268a8d261261409ce99adc00d1e0eb192
YasmimVieira <vieiray937@gmail.com>
2021-02-24 21:39:56 -0300

    chore: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page', loadChildren: () => import('./page-error/page-error.module').then(m => m.PageErrorModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/page-error/page-error-routing.module.ts
+++ src/app/page-error/page-error-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageErrorComponent } from './page-error.component';
+
+const routes: Routes = [
+  { path: 'error', component: PageErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
