d1748ccdc8ed2007fcf14c8dcc4dbc4969243c92
YasmimVieira <vieiray937@gmail.com>
2021-02-24 21:34:23 -0300

    chore: create component page error

---
+++ src/app/page-error/page-error.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageErrorRoutingModule } from './page-error-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PageErrorRoutingModule
+  ]
+})
+export class PageErrorModule { }
