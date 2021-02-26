81cc003d1add77763a9dd43d2e94e5a228544e29
YasmimVieira <vieiray937@gmail.com>
2021-02-26 16:53:19 -0300

    chore: create component latest realease

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { AccountCreditComponent } from './account-credit/account-credit.component';
+import { CardComponent } from './card/card.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CardComponent } from './card/card.component';
-import { AccountCreditComponent } from './account-credit/account-credit.component';
 import { LastestReleaseComponent } from './lastest-release/lastest-release.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, CardComponent, AccountCreditComponent, LastestReleaseComponent],
+  declarations: [
+    DashboardComponent, 
+    CardComponent, 
+    AccountCreditComponent, 
+    LastestReleaseComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
