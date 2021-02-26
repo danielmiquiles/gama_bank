5304e00618c698767aaaba491a353f59dcffca52
Daniel Miquiles <danielvictormiquiles@gmail.com>
2021-02-26 20:09:44 -0300

    fix: router link not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoAuthGuard } from './shared/guards/no-auth.guard';
 
-
 const routes: Routes = [
-  { path: 'page', loadChildren: () => import('./page-error/page-error.module').then(m => m.PageErrorModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
-  { path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: '**', component: NotFoundComponent }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'page',
+    loadChildren: () =>
+      import('./page-error/page-error.module').then((m) => m.PageErrorModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./components/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./components/login/login.module').then( m => m.LoginModule),
+    canActivate: [NoAuthGuard]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
