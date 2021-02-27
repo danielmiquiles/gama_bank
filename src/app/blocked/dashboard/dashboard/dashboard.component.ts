15a865ec7d8446f113d8b9b9155b2b8b18736ecd
YasmimVieira <vieiray937@gmail.com>
2021-02-27 00:49:24 -0300

    chore: get user in component dashboard

--- src/app/blocked/dashboard/dashboard/dashboard.component.ts
+++ src/app/blocked/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/interfaces/user.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  user: User
+
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getUsuario()
   }
 
 }
