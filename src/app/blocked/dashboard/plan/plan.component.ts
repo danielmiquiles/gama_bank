63be02f534420d48b3ea72f1d21c1efba1b38a40
YasmimVieira <vieiray937@gmail.com>
2021-02-28 00:07:51 -0300

    chore: list plans

--- src/app/blocked/dashboard/plan/plan.component.ts
+++ src/app/blocked/dashboard/plan/plan.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize, take } from 'rxjs/operators';
+import { Plans } from 'src/app/shared/interfaces/plans';
+
+import { PlanService } from './plan.service';
 
 @Component({
   selector: 'app-plan',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean;
+  plans: Plans[];
+
+  constructor(
+    private planService: PlanService
+  ) { }
 
   ngOnInit(): void {
+    this.loadingPlans();
   }
 
+  loadingPlans() {
+    this.isLoading = true;
+
+    this.planService.getPlans()
+    .pipe(
+      take(1),
+      finalize(() => this.isLoading = false)
+    )
+    .subscribe(
+      response => this.onSuccess(response)
+    )
+  }
+
+  onSuccess(response: Plans[]) {
+    this.plans = response;
+  }
 }
--- src/app/blocked/dashboard/plan/plan.service.ts
+++ src/app/blocked/dashboard/plan/plan.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Plans } from 'src/app/shared/interfaces/plans';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPlans() {
+    return this.http.get<Plans[]>(`${this.API_URL}/lancamentos/planos-conta`)
+  }
 }
