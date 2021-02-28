e43dcd92ca589420aed996cf00ce5a6422d5a977
YasmimVieira <vieiray937@gmail.com>
2021-02-28 02:37:19 -0300

    chore: add service plan

--- src/app/blocked/dashboard/plan/plan.service.ts
+++ src/app/blocked/dashboard/plan/plan.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Plans } from 'src/app/shared/interfaces/plans';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -11,10 +12,21 @@ export class PlanService {
   API_URL = environment.API_URL;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private token: AuthService
   ) { }
 
+
   getPlans() {
-    return this.http.get<Plans[]>(`${this.API_URL}/lancamentos/planos-conta`)
+      const user = this.token.getUsuario();
+      let headers = new HttpHeaders({
+        'Authorization': this.token.getToken()
+      });
+      let params = new HttpParams().set('login', user.login);
+      let httpOptions = {
+        headers: headers,
+        params: params
+      };
+      return this.http.get<Plans[]>(`${this.API_URL}/lancamentos/planos-conta`, httpOptions)
   }
 }
